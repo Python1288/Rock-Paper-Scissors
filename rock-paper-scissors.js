ef1afb4a28ffa7bc963ff0cdc38f81601b844ff7
Adzi <belkoadam@gmail.comm>
2022-05-22 17:20:00 +1000

    functions  computerPlay playRound and game working. Implementation of score after each round needed

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,8 +10,11 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let score;
+
     if (computerSelection == 0 && playerSelection == "rock") {
-        return console.log("Draw, you both have rock!");
+        console.log("Draw, you both have rock!");
+        return score = draw;
 
     } else if (computerSelection == 1 && playerSelection == "paper") {
         return console.log("Draw, you both have paper!");
@@ -20,7 +23,8 @@ function playRound(playerSelection, computerSelection) {
         return console.log("Draw, you both have scissors!");
 
     } else if (computerSelection == 0 && playerSelection == "paper") {
-        return console.log("You win! Paper beats rock!");
+         console.log("You win! Paper beats rock!");
+         return score = win;
 
     } else if (computerSelection == 1 && playerSelection == "scissors") {
          return console.log("You win! Scissors beats paper!");
@@ -38,7 +42,15 @@ function playRound(playerSelection, computerSelection) {
         return console.log("You Lose! Paper beats rock!");
     }
 }
-console.log("\n");
-const playerSelection = prompt("Your Choice: ")
-const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection);
+
+function game() {
+        for (let i=0; i<5; i++) {
+            console.log("\n");
+            const playerSelection = prompt("Your Choice: ").toLowerCase();
+            const computerSelection = computerPlay();
+            playRound(playerSelection, computerSelection);
+    }
+}
+
+
+game();
