b6d7c9d226ec53b212c9e2a9fe110196f9863043
Adzi <belkoadam@gmail.comm>
2022-06-17 17:47:40 +1000

    js functionality done

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,94 +1,86 @@
-"use strict";
-const ps = require("prompt-sync");
-const prompt = ps();
 
-// Generating random number ala rock paper scissors
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+const info = document.querySelector(".info");
+const results = document.querySelector(".results");
+info.appendChild(results);
+
+const score = document.querySelector(".score");
+info.appendChild(score);
+
+const verdict = document.querySelector(".verdict");
+info.appendChild(verdict);
+
+let countRound = 0;
+let countComp = 0;
+let countHuman = 0;
+
+for (const btn of buttons) {
+    btn.addEventListener("click", (e) => game(e.target.id));
+ }
+ 
+ function disableButton() {
+     document.getElementById("rock").disabled = true;
+     document.getElementById("paper").disabled = true;
+     document.getElementById("scissors").disabled = true;
+ }
 
 function computerPlay() {
     const rps = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * rps.length);
-    console.log("Computers pick: " + (rps[random]));
     return random;
 }
 
-// The game, humans pick vs computers pick, returning result into variable
+function game(id) {
+    const playerSelection = id;
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    score.textContent = "Score: " + countComp + " : " + countHuman;
+
+    if (countComp == 5) {
+        verdict.textContent = "Sorry, you lost the game!"
+        disableButton();
+        
+
+    } else if (countHuman == 5) {
+        verdict.textContent = "Congratz! You win the game!" 
+        disableButton();
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
     if (computerSelection == 0 && playerSelection == "rock") {
-        return console.log("Draw, you both have rock!");
+        results.textContent = "Draw, you both have rock!";
 
     } else if (computerSelection == 1 && playerSelection == "paper") {
-        return console.log("Draw, you both have paper!");
+        results.textContent = "Draw, you both have paper!";
 
     } else if (computerSelection == 2 && playerSelection == "scissors") {
-        return console.log("Draw, you both have scissors!");
+        results.textContent = "Draw, you both have scissors!";
 
     } else if (computerSelection == 0 && playerSelection == "paper") {
-        console.log("You win! Paper beats rock!");
+        results.textContent = "You win! Paper beats rock!";
         return countHuman += 1;
 
     } else if (computerSelection == 1 && playerSelection == "scissors") {
-        console.log("You win! Scissors beats paper!");
+        results.textContent = "You win! Scissors beats paper!";
         return countHuman += 1;
     
     } else if (computerSelection == 2 && playerSelection == "rock") {
-        console.log("You win! Rock beats scissors!");
+        results.textContent = "You win! Rock beats scissors!";
         return countHuman += 1;
 
     } else if (computerSelection == 0 && playerSelection == "scissors") {
-        console.log("You Lose! Rock beats scissors!");
+        results.textContent = "You Lose! Rock beats scissors!";
         return countComp += 1;
 
-    } else if (computerSelection == 2 && playerSelection == "paper") {
-        console.log("You Lose! Scissors beats paper!");
+    } else if (computerSelection == 2 && playerSelection == paper) {
+        results.textContent = "You Lose! Scissors beats paper!";
         return countComp += 1;
     
-    } else if (computerSelection == 1 && playerSelection == "rock"){
-        console.log("You Lose! Paper beats rock!");
+    } else if (computerSelection == 1 && playerSelection == rock){
+        results.textContent = "You Lose! Paper beats rock!";
         return countComp += 1;
-
-    } else {
-        console.log("What are you doing?")
-    }
+    } 
 }
-
-// loop of 5 rounds, the winner is decided after the loop is finished
-
-function game() {
-
-        for (let i=0; i<5; i++) {
-            console.log("\n");
-            console.log("Round " + (countRound += 1) + ": ");
-            const playerSelection = prompt("Your pick: ").toLowerCase();
-            const computerSelection = computerPlay();
-            playRound(playerSelection, computerSelection);
-            console.log("Score: " + countComp + " : " + countHuman);
-    }
-
-    if (countComp > countHuman) {
-        console.log("Sorry, you lost the game!\n")
-
-    } else if (countComp < countHuman) {
-        console.log("Congratz! You win the game!\n")
-
-    } else {
-        console.log("It's a draw!\n")
-    }
-
-    const repeatQ = prompt("Would you like to play this game again? (yes/no): ").toLowerCase();
-        if (repeatQ == "yes") {
-            countRound = 0;
-            countComp = 0;
-            countHuman = 0;
-            game();
-
-        } else {
-            console.log("Game finished, have a good day!");
-   }
-}
-
-console.log("\nWelcome to the game called Paper Scissors Rock! Please choose one to begin the game.");
-let countRound = 0;
-let countComp = 0;
-let countHuman = 0;
-game();
